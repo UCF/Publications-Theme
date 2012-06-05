afa5654b9b036c43e35f45474ec77c6c5db8b0d3
Jo Greybill <jogreybill@gmail.com>
2012-06-05 10:53:51 -0400

    Basic setup for single publication

--- taxonomy-publications.php
+++ taxonomy-publications.php
@@ -5,17 +5,24 @@
 	$latestEdition = get_posts(array('post_type' => 'pubedition', 'taxonomy' => 'publications', 'term' => $publication->name, 'order' => 'DESC', 'post_status' => 'publish', 'numberposts' => 1));
 	$latestEdition = $latestEdition[0];
 	?>
-	
-	<div class="publication-content" id="<?=$latestEdition->post_slug?>">
+	<div class="publication-content" id="<?=$latestEdition->post_name?>">
 	
 		<div class="row" id="subpage-nav">
 			<a class="btn" href="<?=get_site_url()?>"><i class="icon-chevron-left"></i> Back to All Publications</a>
 		</div>
 	
 		<div class="row">
-			<h2><?=$latestEdition->post_title?></h2>
-			
+			<div class="span4">
+				<h2><?=$latestEdition->post_title?></h2>
+				<a target="_blank" class="btn btn-primary" href="<?=get_post_meta($latestEdition->ID, 'pubedition_url', TRUE)?>">Launch Online Viewer</a>
+			</div>
+			<div class="span8">
+				<?php
+				echo get_the_post_thumbnail($latestEdition->ID, 'large', array('class' => 'publication-single-image'));
+				?>
+			</div>
 		</div>
+		
 	</div>	
 	
 <?php get_footer(); ?>
