5cb4dc6c9d81f2d84a3977b3b1eb3f30ae9a30d5
Jo Greybill <jogreybill@gmail.com>
2012-06-07 14:18:31 -0400

    Added <h2>, etc

--- page.php
+++ page.php
@@ -1,4 +1,11 @@
 <?php disallow_direct_load('page.php');?>
-<?php get_header(); the_post();?>
-<?php the_content(); ?>
+<?php get_header();?>
+	<div class="page-content" id="page-single" data-template="page">
+		<div class="row">
+			<div class="span12">
+				<h2><?php the_title(); ?></h2>
+			</div>
+			<?php the_content(); ?>
+		</div>
+	</div>
 <?php get_footer();?>
