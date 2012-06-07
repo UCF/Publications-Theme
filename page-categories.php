e260773e710e752df7b8014c56937d11a285aee8
Jo Greybill <jogreybill@gmail.com>
2012-06-07 14:15:46 -0400

    Removed post count from cat lists as the number is incorrect

--- page-categories.php
+++ page-categories.php
@@ -5,7 +5,7 @@
 				
 				<?php 					
 					print "<div class='span12' id='allcats_list'><h2>All Categories</h2>";
-					wp_list_categories(array('title_li' => '', 'style' => 'none', 'show_count' => 1));
+					wp_list_categories(array('title_li' => '', 'style' => 'none'));
 					print "</div>";
 				?>
 				
