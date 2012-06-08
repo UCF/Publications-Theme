f8cee220c4148fca294db1cc377e6e540184e337
Jo Greybill <jogreybill@gmail.com>
2012-06-08 10:12:29 -0400

    Set up single pubeditions to display at full-size as a fallback to publication output

--- single.php
+++ single.php
@@ -1,1 +1,5 @@
-<?php disallow_direct_load('single.php');?>
+<?php get_header(); ?>
+<div class="pubedition_wrap">
+	<?php echo apply_filters('the_content', get_post_meta($post->ID, 'pubedition_embed', TRUE)); ?>
+</div>
+<?php get_footer(); ?>
