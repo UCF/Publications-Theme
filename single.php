fd407732beee036655710f0f8e286c84ffc872bf
Jo Greybill <jogreybill@gmail.com>
2012-06-08 10:24:11 -0400

    Changed pubedition_wrap to publication_wrap for consistency

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div class="pubedition_wrap">
+<div class="publication_wrap">
 	<?php echo apply_filters('the_content', get_post_meta($post->ID, 'pubedition_embed', TRUE)); ?>
 </div>
 <?php get_footer(); ?>
