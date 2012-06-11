e9380c42ff554092ba00d689179a94ce4e325827
Jo Greybill <jogreybill@gmail.com>
2012-06-11 12:36:25 -0400

    Category lists now display the correct number of publications

--- page-categories.php
+++ page-categories.php
@@ -5,8 +5,50 @@
 				
 				<?php 					
 					print "<div class='span12' id='allcats_list'><h2>All Categories</h2>";
-					wp_list_categories(array('title_li' => '', 'style' => 'none'));
+					
+					/*
+					 * Print all categories and the number of unique publications (taxonomy terms) per category:
+					 */ 
+					
+					//Set up empty array for count results (will contain 'pubname' and 'category'):
+					$postpubs = array();
+					
+					//Then get categories:					
+					$cats = get_categories();
+					foreach ($cats as $cat) {
+						
+						//For each category, get all pubeditions and store their publication and category information in $postpubs
+						$posts = get_posts(array('post_type' => 'pubedition', 'category' => $cat->term_id, 'order' => 'DESC', 'post_status' => 'publish', 'numberposts' => -1));
+						
+						foreach ($posts as $post) {
+							$post_publication = get_the_terms( $post->ID, 'publications' );
+							foreach ($post_publication as $the_post_publication) {
+								$post_publication_name = $the_post_publication->name;
+								$postpubs[] = array('pubname' => $post_publication_name, 'cat' => $cat->name);
+							}
+						}
+					}
+					
+					//Remove duplicate $postpub entries to avoid a miscount
+					$postpubs = array_map("unserialize", array_unique(array_map("serialize", $postpubs)));
+					
+					//Finally, get the count and spit it out:
+					foreach ($cats as $cat) {
+						$count = 1;
+						foreach ($postpubs as $postpub) {
+							if ($postpub['cat'] == $cat->name) {
+								$count = $count + 1;
+							}
+						}
+						$count = $count - 1;
+						print "<a href='";
+						print get_category_link($cat->term_id);
+						print "'>".$cat->name." (".$count.")</a><br/>";
+					}
+					
+					
 					print "</div>";
+					
 				?>
 				
 		</div>
