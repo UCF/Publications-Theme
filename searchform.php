1a2d7c3dcc0d95f45b4b5f4cdc008d25f11e9def
Jo Greybill <jogreybill@gmail.com>
2012-06-11 16:45:11 -0400

    Changed label name to 'search publications'

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?=home_url( '/' )?>">
 	<div>
-		<label for="s" id="searchlabel">Search:</label>
+		<label for="s" id="searchlabel">Search Publications:</label>
 		<input type="text" value="<?=htmlentities($_GET['s'])?>" name="s" class="search-field" id="s" placeholder="Enter your search term here..." />
 		<button type="submit" class="search-submit">Search</button>
 	</div>
