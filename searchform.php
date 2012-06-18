8c6665a947ac6f2ecd91993f343b6216bf0e6681
Jo Greybill <jogreybill@gmail.com>
2012-06-18 14:33:17 -0400

    Changed search form placeholder to 'search publications'; re-positioned search form and links in footer

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?=home_url( '/' )?>">
 	<div>
 		<label for="s" id="searchlabel">Search Publications:</label>
-		<input type="text" value="<?=htmlentities($_GET['s'])?>" name="s" class="search-field" id="s" placeholder="Enter your search term here..." />
+		<input type="text" value="<?=htmlentities($_GET['s'])?>" name="s" class="search-field" id="s" placeholder="Search Publications" />
 		<button type="submit" class="search-submit">Search</button>
 	</div>
 </form>
