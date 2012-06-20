bdd4efa331c40677c2722d84ea36e4938ae3c9bf
Jo Greybill <jogreybill@gmail.com>
2012-06-20 16:05:15 -0400

    Condensed embed display code into a single function

--- single.php
+++ single.php
@@ -20,33 +20,9 @@ foreach ($latestEditions as $latestEdition) {
 <div class="publication_wrap">
 
 	<?php 
-	$shortcode 		   	  	= get_post_meta($post->ID, 'pubedition_embed', TRUE);
-	$embedcode_explode_id 	= preg_split("/documentId=/", $shortcode); //split up shortcode at documentid
-	$embedcode_explode_id 	= preg_split("/ name=/", $embedcode_explode[1]); //remove the rest of the embed code, leaving the document id
-	$docID 			   	  	= $embedcode_explode_id[0];
-	
-	$embedcode_explode_name = preg_split("/name=/", $shortcode); //split up shortcode at name
-	$embedcode_explode_name	= preg_split("/ username=/", $embedcode_explode[1]); //remove the rest of the embed code, leaving the name
-	$docname = $embedcode_explode_name[0];
-	
-	if( (strstr($_SERVER['HTTP_USER_AGENT'],"iPad")) || (strstr($_SERVER['HTTP_USER_AGENT'],"iPhone")) || (strstr($_SERVER['HTTP_USER_AGENT'],"iPod")) ) {
+	$shortcode = apply_filters('the_content', get_post_meta($post->ID, 'pubedition_embed', TRUE));
+	embed_issuu($shortcode); 
 	?>
 	
-	<div class="row">
-		<div class="span5">
-			<img src='http://image.issuu.com/<?=$docID?>/jpg/page_1_thumb_large.jpg' alt='<?=$post->post_title?>' title='<?=$post->post_title?>' />
-		</div>
-		<div class="span7">
-			<h2><?=$post->post_title?></h2>
-			<p>Hello, <?=$_SERVER['HTTP_USER_AGENT']?> user!</p>
-			<p><a href="http://issuu.com/universityofcentralflorida/docs/<?=$docname?>?mode=mobile">Click here</a> to access this publication on your device.</p>
-		</div>
-	</div>
-	
-	<?php 
-	} else {
-		echo apply_filters('the_content', $shortcode); 
-	}
-	?>
 </div>
 <?php get_footer(); ?>
