ed5774fa6f6b9a120fd1dea59c4aadfdaafc278f
Jo Greybill <jogreybill@gmail.com>
2012-11-05 16:26:13 -0500

    Updated single publication template to not include device width meta tag for better size flexibility in modals on ipad

--- taxonomy-publications.php
+++ taxonomy-publications.php
@@ -1,14 +1,44 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html style="width:100%; height:100%;">
+	<head>
+		<?="\n".header_()."\n"?>
+		<?php if(GA_ACCOUNT or CB_UID):?>
 
-	<?php
-	$publication = $wp_query->queried_object;
-	$latestEdition = get_posts(array('post_type' => 'pubedition', 'taxonomy' => 'publications', 'term' => $publication->name, 'order' => 'DESC', 'post_status' => 'publish', 'numberposts' => 1));
-	$latestEdition = $latestEdition[0];
-	?>
-
-	<?php 
-	$shortcode = get_post_meta($latestEdition->ID, 'pubedition_embed', TRUE);
-	embed_issuu($shortcode, $publication->name); 
-	?>
+		<script type="text/javascript">
+			var _sf_startpt = (new Date()).getTime();
+			<?php if(GA_ACCOUNT):?>
+			
+			var GA_ACCOUNT = '<?=GA_ACCOUNT?>';
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', GA_ACCOUNT]);
+			_gaq.push(['_setDomainName', 'none']);
+			_gaq.push(['_setAllowLinker', true]);
+			_gaq.push(['_trackPageview']);
+			<?php endif;?>
+			<?php if(CB_UID):?>
 	
-<?php get_footer(); ?>
+			var CB_UID = '<?=CB_UID?>';
+			var CB_DOMAIN = '<?=CB_DOMAIN?>';
+			<?php endif?>
+		</script>
+		<?php endif;?>
+	</head>
+	
+	<body class="<?=get_post_type($post->ID)?>">
+	
+		<?php
+		$publication = $wp_query->queried_object;
+		$latestEdition = get_posts(array('post_type' => 'pubedition', 'taxonomy' => 'publications', 'term' => $publication->name, 'order' => 'DESC', 'post_status' => 'publish', 'numberposts' => 1));
+		$latestEdition = $latestEdition[0];
+		?>
+	
+		<?php 
+		$shortcode = get_post_meta($latestEdition->ID, 'pubedition_embed', TRUE);
+		embed_issuu($shortcode, $publication->name); 
+		?>
+	
+	</body>
+	<!--[if IE]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</html>
