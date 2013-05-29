dd95caa16ac855116756225c9fb6614667fb02be
Jo Greybill <jogreybill@gmail.com>
2013-05-29 09:38:59 -0400

    better logic for looping through categories and publications

--- page-categories.php
+++ page-categories.php
@@ -11,41 +11,39 @@
 					 */ 
 					
 					//Set up empty array for count results (will contain 'pubname' and 'category'):
-					$postpubs = array();
+					$publication_cats = array();
 					
 					//Then get categories:					
-					$cats = get_categories();
-					foreach ($cats as $cat) {
-						
-						//For each category, get all pubeditions and store their publication and category information in $postpubs
+					$all_cats = get_categories();
+					foreach ($all_cats as $cat) {
+						//For each category, get all pubeditions and store their publication and category information in $publication_cats
 						$posts = get_posts(array('post_type' => 'pubedition', 'category' => $cat->term_id, 'order' => 'DESC', 'post_status' => 'publish', 'numberposts' => -1));
 						
 						foreach ($posts as $post) {
 							$post_publications = get_the_terms( $post->ID, 'publications' );
 							if (is_array($post_publications)) {
 								foreach ($post_publications as $post_publication) {
-									$post_publication_name = $post_publication->name;
-									$postpubs[] = array('pubname' => $post_publication_name, 'cat' => $cat->name);
+									if (!isset($publication_cats[$post_publication->name])) {
+										$publication_cats[$post_publication->name] = $cat->name;
+									}
 								}
 							}
 						}
 					}
 					
-					//Remove duplicate $postpub entries to avoid a miscount
-					$postpubs = array_map("unserialize", array_unique(array_map("serialize", $postpubs)));
-					
 					//Finally, get the count and spit it out:
-					foreach ($cats as $cat) {
-						$count = 1;
-						foreach ($postpubs as $postpub) {
-							if ($postpub['cat'] == $cat->name) {
-								$count = $count + 1;
+					foreach ($all_cats as $cat) {
+						$count = 0;
+						foreach ($publication_cats as $publication=>$publication_catname) {
+							if ($cat->name == $publication_catname) {
+								$count++;
 							}
 						}
-						$count = $count - 1;
-						print "<a href='";
-						print get_category_link($cat->term_id);
-						print "'>".$cat->name." (".$count.")</a><br/>";
+						if ($count > 0) {
+							print "<a href='";
+							print get_category_link($cat->term_id);
+							print "'>".$cat->name." (".$count.")</a><br/>";
+						}
 					}
 					
 					
